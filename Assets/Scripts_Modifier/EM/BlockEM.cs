f8a3129e1ab9acd7e5fc4f4aece6d4c8af02253b
onovich <hjn1110@163.com>
2024-04-28 21:00:10 +0800

    <feature> add: BlockEM / BlockShapeEM

---
+++ Assets/Scripts_Modifier/EM/BlockEM.cs
@@ -0,0 +1,76 @@
+#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace Oshi {
+
+    public class BlockEM : SerializedMonoBehaviour {
+
+        [Header("Bake Target")]
+        public BlockTM blockTM;
+
+        [Header("Block Info")]
+        public int typeID;
+        public string typeName;
+
+        [Header("Block Mesh")]
+        public Sprite mesh;
+        public UnityEngine.Color color;
+        public Material meshMaterial;
+
+        [Header("Block Shapes")]
+        public ShapeTM[] shapes;
+
+        [Button("Load")]
+        void Load() {
+            typeID = blockTM.typeID;
+            typeName = blockTM.typeName;
+            mesh = blockTM.mesh;
+            color = blockTM.meshColor;
+            meshMaterial = blockTM.meshMaterial;
+            GetShapes();
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+        }
+
+        void GetShapes() {
+            if (shapes == null) return;
+            if (blockTM == null) return;
+            if (blockTM.shapeArr == null) return;
+            shapes = new ShapeTM[blockTM.shapeArr.Length];
+            for (int i = 0; i < blockTM.shapeArr.Length; i++) {
+                var shape = blockTM.shapeArr[i];
+                shapes[i] = shape;
+            }
+        }
+
+        void BakeShapes() {
+            var shapeList = new List<ShapeTM>();
+            for (int i = 0; i < shapes.Length; i++) {
+                var shape = shapes[i];
+                shapeList.Add(shape);
+            }
+            blockTM.shapeArr = shapeList.ToArray();
+        }
+
+        [Button("Bake")]
+        void Bake() {
+            blockTM.typeID = typeID;
+            blockTM.typeName = typeName;
+            blockTM.mesh = mesh;
+            blockTM.meshColor = color;
+            blockTM.meshMaterial = meshMaterial;
+            BakeShapes();
+            EditorUtility.SetDirty(blockTM);
+            AssetDatabase.SaveAssets();
+        }
+
+    }
+
+}
+#endif
---
+++ Assets/Scripts_Modifier/EM/BlockShapeEM.cs
@@ -0,0 +1,92 @@
+#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace Oshi {
+
+    public class BlockShapeEM : SerializedMonoBehaviour {
+
+        [Header("Bake Target")]
+        public ShapeTM shapeTM;
+
+        [Header("Block Cells")]
+        public Vector2Int sizeInt;
+
+        [Button("Load")]
+        void Load() {
+            sizeInt = shapeTM.sizeInt;
+            GetCells();
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+        }
+
+        void BakeCells() {
+            List<Vector2Int> cellList = new List<Vector2Int>();
+            for (int x = 0; x < sizeInt.x; x++) {
+                for (int y = 0; y < sizeInt.y; y++) {
+                    if (cells[x, y]) {
+                        cellList.Add(new Vector2Int(x, sizeInt.y - 1 - y));
+                    }
+                }
+            }
+            shapeTM.cells = cellList.ToArray();
+        }
+
+        [Button("Bake")]
+        void Bake() {
+            shapeTM.sizeInt = sizeInt;
+            BakeCells();
+            EditorUtility.SetDirty(shapeTM);
+            AssetDatabase.SaveAssets();
+        }
+
+        void GetCells() {
+            cells = new bool[sizeInt.x, sizeInt.y];
+            for (int i = 0; i < shapeTM.cells.Length; i++) {
+                var x = shapeTM.cells[i].x;
+                var y = sizeInt.y - 1 - shapeTM.cells[i].y;
+                cells[x, y] = true;
+            }
+        }
+
+        [Button("Clear")]
+        void Clear() {
+            cells = new bool[sizeInt.x, sizeInt.y];
+        }
+
+        [Button("Resize")]
+        void Resize() {
+            var newCells = new bool[sizeInt.x, sizeInt.y];
+            for (int x = 0; x < Mathf.Min(sizeInt.x, cells.GetLength(0)); x++) {
+                for (int y = 0; y < Mathf.Min(sizeInt.y, cells.GetLength(1)); y++) {
+                    newCells[x, y] = cells[x, y];
+                }
+            }
+            cells = newCells;
+        }
+
+        [TableMatrix(DrawElementMethod = "DrawCell", SquareCells = true)]
+        public bool[,] cells;
+
+        bool DrawCell(Rect rect, bool value) {
+            if (Event.current.type == EventType.MouseDown &&
+            rect.Contains(Event.current.mousePosition)) {
+                value = !value;
+                GUI.changed = true;
+                Event.current.Use();
+            }
+            EditorGUI.DrawRect(
+                rect.Padding(1),
+                value ? new UnityEngine.Color(0.1f, 0.8f, 0.2f) :
+                new UnityEngine.Color(0, 0, 0, 0.5f));
+            return value;
+        }
+    }
+
+}
+#endif
